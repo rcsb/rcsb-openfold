279cf4de3f72086d47d6e9f4e26ef37704343fd2
Jose Duarte <jose.m.duarte@gmail.com>
2023-02-17 16:36:00 -0800

    Some fixes

--- scripts/expanse/run_of_inference_expanse.sh
+++ scripts/expanse/run_of_inference_expanse.sh
@@ -2,8 +2,8 @@
 # parameters taken from AlphaFold example expanse script
 # note the project "SDS154" I took from my initial email
 #SBATCH --job-name openfold
-#SBATCH --output="openfold.%j.%N.out"
-#SBATCH --error="openfold.%j.%N.err"
+#SBATCH --output="openfold.%j.%N.log"
+#SBATCH --error="openfold.%j.%N.log"
 #SBATCH --account=SDS154
 #SBATCH --time=08:00:00
 #SBATCH --gpus=1
@@ -14,5 +14,5 @@
 #SBATCH --mem=40G
 
 echo "Start: $(date)"
-/home/jmduarte/run_of_inference.sh
+$(pwd)/run_of_inference.sh "$1"
 echo "Done:  $(date)"
