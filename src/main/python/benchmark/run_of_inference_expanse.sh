035a4e614cc3f2c33b37aa165b58b3a50affd8cb
josemduarte <jose.m.duarte@gmail.com>
2023-04-11 10:19:28 -0700

    Adding the new project name and some docs

--- src/main/python/benchmark/run_of_inference_expanse.sh
+++ src/main/python/benchmark/run_of_inference_expanse.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# parameters taken from AlphaFold example expanse script
-# note the project "SDS154" I took from my initial email
+# Initial parameters taken from AlphaFold example expanse script
+# Projects:
+## Initial trial project for user jmduarte: "SDS154" (I took from my initial email)
+## The "ACCESS Explore" project applied for benchmarking OpenFold on homomers: BIO230048 (ACCESS project name), TG-BIO230048 (ACCESS allocation id), SDS194 (Expanse project name)
+#
+# To find remaining credits use: expanse-client user jmduarte -p -r expanse_gpu
+# To find Lustre project quota use: lfs quota -g sds194 -h /expanse/lustre/projects/sds194
+#
 #SBATCH --job-name openfold
 #SBATCH --output="openfold.%j.%N.log"
 #SBATCH --error="openfold.%j.%N.log"
-#SBATCH --account=SDS154
+#SBATCH --account=SDS194
 #SBATCH --time=08:00:00
 #SBATCH --gpus=1
 #SBATCH --nodes=1
