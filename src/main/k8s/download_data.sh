27b4d4f1241025b86d0156e7011c9f48b866e0bb
josemduarte <jose.m.duarte@gmail.com>
2023-04-14 16:33:08 -0700

    Stub for data download

---
+++ src/main/k8s/download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+vol=/ofvol
+
+mkdir $vol/input
+mkdir $vol/input/sequences
+mkdir $vol/input/alignments
+mkdir $vol/templates
+mkdir $vol/params
+mkdir $vol/output_models
+
+# weights
+
+# alignments
+
+# inputs
